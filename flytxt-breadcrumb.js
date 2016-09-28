821796cd35927a168aa4cd8056c6f3d960f09458
priyanka <priyanka.chaliyan@flytxt.com>
2016-09-28 12:23:56 +0530

    Add dependency
    Remove unnecessary behaviors

--- flytxt-breadcrumb.js
+++ flytxt-breadcrumb.js
@@ -1,8 +1,6 @@
 Polymer({
   is: 'flytxt-breadcrumb',
   
-  behaviors: [Flytxt.webL10n, Flytxt.TemplateHelper],
-  
   properties: {
     href: {
       type: String,
@@ -14,9 +12,14 @@ Polymer({
     }
   },
   
-  switchUrl: function() {
+  switchUrl: function(e) {
     if (this.href) {
       app.$.router.go(this.href);
     }
-  }
+    e.stopPropagation();
+  },
+  
+  elvis: function(condition, success, failure) {
+    return condition ? success : failure;
+  },
 });
